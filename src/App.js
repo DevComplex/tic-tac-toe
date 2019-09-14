cde0e06897048f7b60ea46384833ee8fd706de70
Pedram T'Kanchi <cs.wizardry@gmail.com>
2019-09-14 13:43:20 -0700

    Fixed Game component missname in the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Board from './Game';
+import Game from './Game';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <Board/>
+      <Game/>
     </div>
   );
 }
